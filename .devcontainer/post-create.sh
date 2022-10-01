9717a312190e69a0879f636db454cb1c85d06a45
Justin Yoo <justin.yoojh@gmail.com>
2022-10-01 07:59:58 +0000

    Update devcontainer

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -75,6 +75,8 @@ sudo chmod +x /usr/local/bin/oh-my-posh
 curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/p10k-with-clock.omp.json > $HOME/p10k-with-clock.omp.json
 curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/p10k-without-clock.omp.json > $HOME/p10k-without-clock.omp.json
 curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/switch-p10k-clock.ps1 > $HOME/switch-p10k-clock.ps1
+
+mkdir $HOME/.config/powershell
 curl https://raw.githubusercontent.com/justinyoo/devcontainers-dotnet/main/oh-my-posh/Microsoft.PowerShell_profile.ps1 > $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
 
 cp $HOME/p10k-with-clock.omp.json $HOME/p10k.omp.json
