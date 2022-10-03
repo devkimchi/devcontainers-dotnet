8762870418635038052bab178f076aa35ab4a20b
Justin Yoo <justin.yoojh@gmail.com>
2022-10-03 11:15:12 +0000

    Update devcontainer

--- azure-functions/Build-LocalSettingsJson.ps1
+++ azure-functions/Build-LocalSettingsJson.ps1
@@ -1,4 +1,66 @@
 ### This add the GitHub Codespaces capability to your local.settings.json
+Param(
+    [switch]
+    [Parameter(Mandatory=$false)]
+    $UseNgrok,
+
+    [switch]
+    [Parameter(Mandatory=$false)]
+    $Help
+)
+
+function Show-Usage {
+    Write-Output "
+    Usage: $(Split-Path $MyInvocation.ScriptName -Leaf) [-WithClock] [-Help]
+
+    Options:
+        -UseNgrok:  This switch indicates whether to use the ngrok URL or not
+        -Help:      Show this message.
+
+"
+
+    Exit 0
+}
+
+# Show usage
+if ($Help -eq $true) {
+    Show-Usage
+    Exit 0
+}
+
+function Get-CodespaceUrl {
+    param (
+        [string] $CodespaceName
+    )
+
+    $codespaceUrl = "https://$($CodespaceName)-7071.githubpreview.dev/api"
+
+    return $codespaceUrl
+}
+
+function Get-NgrokUrl {
+    $tunnels = $(ngrok api tunnels list | ConvertFrom-Json).tunnels
+    $ngrokUrl = "$($($tunnels | Where-Object { $_.forwards_to -eq "http://localhost:7071" }).public_url)/api"
+
+    return $ngrokUrl
+}
+
+function Get-HostUrls {
+    param (
+        [string] $CodespaceName,
+        [bool] $UseNgrok
+    )
+
+    $ngrokUrl = Get-NgrokUrl
+    $codespaceUrl = Get-CodespaceUrl -CodespaceName $CodespaceName
+    $url = $codespaceUrl
+    if ($UseNgrok -eq $true) {
+        $url = $ngrokUrl
+    }
+    $urls = @{ ngrokUrl = $ngrokUrl; codespaceUrl = $codespaceUrl; url = $url }
+
+    return $urls
+}
 
 # Get the local.settings.sample.json file.
 $localSettingsSampleJson = Get-ChildItem -Path $env:CODESPACE_VSCODE_FOLDER -Filter local.settings.sample.json -Recurse
@@ -6,7 +68,7 @@ $localSettingsSampleJson = Get-ChildItem -Path $env:CODESPACE_VSCODE_FOLDER -Fil
 # Get the local.settings.json file. If not exists, create one.
 $localSettingsJson = Get-ChildItem -Path $localSettingsSampleJson.Directory.FullName -Filter local.settings.json
 if ($localSettingsJson -eq $null) {
-    Copy-Item -Path $localSettingsSampleJson.FullName -Destination "$($localSettingsSampleJson.Directory.Name)/local.settings.json"
+    Copy-Item -Path $localSettingsSampleJson.FullName -Destination "$($localSettingsSampleJson.Directory.FullName)/local.settings.json"
     $localSettingsJson = Get-ChildItem -Path $localSettingsSampleJson.Directory.FullName -Filter local.settings.json
 }
 
@@ -28,10 +90,23 @@ if ($appSettings.Values.OpenApi__RunOnCodespaces -eq $null) {
 }
 
 # Add OpenApi__HostNames to local.settings.json.
+$urls = Get-HostUrls -CodespaceName $env:CODESPACE_NAME -UseNgrok $UseNgrok
 if ($appSettings.Values.OpenApi__HostNames -eq $null) {
-    $appSettings.Values | Add-Member -NotePropertyName OpenApi__HostNames -NotePropertyValue "https://$($env:CODESPACE_NAME)-7071.githubpreview.dev/api"
+    $appSettings.Values | Add-Member -NotePropertyName OpenApi__HostNames -NotePropertyValue $($urls.url)
 } else {
-    $appSettings.Values.OpenApi__HostNames = "https://$($env:CODESPACE_NAME)-7071.githubpreview.dev/api," + $appSettings.Values.OpenApi__HostNames
+    $hostNames = $appSettings.Values.OpenApi__HostNames -split ","
+    $hostNames = [System.Collections.ArrayList]$hostNames
+
+    $ngrokUrls = $hostNames | Where-Object { $_ -like "*ngrok.io/api" }
+    if ($ngrokUrls.Length -gt 0) {
+        $ngrokUrls | ForEach-Object { $hostNames.Remove($_) }
+    }
+    if ($urls.codespaceUrl -ne $null) {
+        $hostNames.Remove($urls.codespaceUrl)
+    }
+    $hostNames.Insert(0, $urls.url)
+
+    $appSettings.Values.OpenApi__HostNames = $($hostNames -join ",").Trim(',')
 }
 
 # Overwrite the existing local.settings.json
