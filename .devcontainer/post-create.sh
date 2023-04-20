a00e0bf4bf1c07e3752ef5b074b97b533f5919a6
Justin Yoo <justin.yoojh@gmail.com>
2023-04-20 14:27:32 +0900

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,9 @@
 ## Install additional apt packages
 sudo apt-get update && \
-    sudo apt-get install -y dos2unix libsecret-1-0 xdg-utils
+#    sudo apt upgrade -y \
+    sudo apt-get install -y dos2unix libsecret-1-0 xdg-utils && \
+    sudo apt clean -y && \
+    sudo rm -rf /var/lib/apt/lists/*
 
 ## Configure git
 git config --global pull.rebase false
